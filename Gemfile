fbc93a037d6aa21a321e87bcf2be8f5cd657f1c6
babrovka <babrovka@gmail.com>
2014-08-12 13:55:50 +0400

    devise for admin

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'inherited_resources'
+gem 'devise'
 
 group :development do
   # Automatically adds annotations to models
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  scope module: 'admin' do
+    devise_for :admins, path: 'admin'
+  end
+  
   namespace :admin do
     resources :products, :taxons, :taxonomies, except: [:show]
   end
