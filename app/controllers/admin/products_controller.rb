22c4c3135a08aaba0d2b2c95920b4d316c822be1
justvitalius <justvitalius@gmail.com>
2014-08-16 22:11:44 +0400

    Add paginations to admin products.

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,9 @@
 class Admin::ProductsController < Admin::ApplicationController
   inherit_resources
 
+  has_scope :page, default: 1, only: :index
+  has_scope :per, default: 3, only: :index
+
   actions :all, except: [:show]
 
 end
