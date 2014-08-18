b9a656a56997a8230b30f031b2e9a976a58e4965
babrovka <babrovka@gmail.com>
2014-08-18 17:23:30 +0400

    friendly url in admin product edit

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,8 @@
 class Admin::ProductsController < Admin::ApplicationController
   inherit_resources
-
+  
+  defaults resource_class: Product.friendly
+  
   has_scope :page, default: 1, only: :index
   has_scope :per, default: 15, only: :index
 
