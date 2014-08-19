cb0ac1e9d000fa9d57be0aa2014911a1b5b99b0d
babrovka <babrovka@gmail.com>
2014-08-19 17:05:29 +0400

    routes, controller actions

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,4 +1,28 @@
 class CheckoutController < ApplicationController
-
+  
+  def cart
+    @cart = current_cart
+  end
+  
+  def checkout
+    if current_cart.line_items.empty?
+      redirect_to root_path, notice: 'Извините, ваша корзина пуста'
+    else
+      @order = Order.new
+    end
+  end
+  
+  def order
+    @order = Order.create(params[:order])
+    @order.add_line_items_from_cart(current_cart)
     
+    if @order.save
+      # отправляем мейлы
+      Cart.destroy(session[:cart_id])
+      session[:cart_id] = nil
+      redirect_to root_path, notice: 'Заказ успешно создан, наши менеджеры свяжуться с вами'
+    else
+      redirect_to root_path, notice: 'Извините, что-то пошло не так'
+    end
+  end
 end
