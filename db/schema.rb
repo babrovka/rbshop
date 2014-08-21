72c1b7c24fe9513077c342dbacedbd115c457e76
babrovka <babrovka@gmail.com>
2014-08-21 11:14:54 +0400

    add_default_taxon_type

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140820080817) do
+ActiveRecord::Schema.define(version: 20140821071345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -439,7 +439,7 @@ ActiveRecord::Schema.define(version: 20140820080817) do
     t.string   "seo_url"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "taxon_type"
+    t.integer  "taxon_type",      default: 0
   end
 
   create_table "stage_images", force: true do |t|
