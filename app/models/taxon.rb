f7b78cb34a02421892e1822069b4128cc2ac6347
babrovka <babrovka@gmail.com>
2014-09-01 12:37:49 +0400

    taxon slug_candidates

--- app/models/taxon.rb
+++ app/models/taxon.rb
@@ -10,11 +10,17 @@ class Taxon < ActiveRecord::Base
   acts_as_nested_set
 
   extend FriendlyId
-  friendly_id :seo_url, use: :slugged
+  friendly_id :slug_candidates, use: :slugged
 
 
   default_scope { order('lft ASC') }
   scope :with_type, -> { where(taxon_type: nil) }
   scope :without, -> (id) { where.not(id: id) }
-
+  
+  def slug_candidates
+    [
+      :seo_url,
+      [:seo_url, :title]
+    ]
+  end
 end
