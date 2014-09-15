3f8902a62888ebf3dafb17010be68cd90b0e299c
babrovka <babrovka@gmail.com>
2014-09-15 14:47:18 +0400

    taxon slug from title

--- app/models/taxon.rb
+++ app/models/taxon.rb
@@ -10,19 +10,12 @@ class Taxon < ActiveRecord::Base
   acts_as_nested_set
 
   extend FriendlyId
-  friendly_id :slug_candidates, use: :slugged
+  friendly_id :title, use: :slugged
 
 
   default_scope { order('lft ASC') }
   scope :with_type, -> { where(taxon_type: nil) }
   scope :without, -> (id) { where.not(id: id) }
-  
-  def slug_candidates
-    [
-      :seo_url,
-      [:seo_url, :title]
-    ]
-  end
   
   def should_generate_new_friendly_id?
     new_record? || slug.blank?
