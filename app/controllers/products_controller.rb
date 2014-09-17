1e8b76fb75086a5943af6269a82ef4c39e8bc64e
justvitalius <justvitalius@gmail.com>
2014-09-17 09:31:41 +0400

    Refactor filter to work with exists routing.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,9 @@
 class ProductsController < ApplicationController
 
-  helper_method :resource, :collection, :current_category
+  helper_method :resource, :collection, :current_category, :filter
   
   def index
+    render_responce
   end
 
   def show
@@ -14,29 +15,41 @@ class ProductsController < ApplicationController
     taxon_ids = Taxon.where(taxonomy_id: selected_taxonomy.id).map(&:id)
     @products = collection.includes(:taxons).where(shop_taxons: { id: taxon_ids })
     seo_data(selected_taxonomy)
-    render :index
+    # render :index
+    render_responce
   end
   
   def taxon
     taxons = selected_taxon.self_and_descendants
     @products = collection.includes(:taxons).where(:shop_taxons => {:id => taxons})
     seo_data(selected_taxon)
-    render :index
+    # render :index
+    render_responce
   end
 
   def filter
     @q = Product.search(params[:q])
-    @products = @q.result(distinct: true)
-    respond_to do |format|
-      format.html { render :index }
-      format.json do
-        txt = Russian.p(@products.count, 'товар', 'товара', 'товаров')
-        render json: { text: "Найдено #{@products.count} #{txt}" }
-      end
-    end
+    # @products = @q.result(distinct: true)
+    # respond_to do |format|
+    #   format.html { render :index }
+    #   format.json do
+    #     txt = Russian.p(@products.count, 'товар', 'товара', 'товаров')
+    #     render json: { text: "Найдено #{@products.count} #{txt}" }
+    #   end
+    # end
   end
 
 private
+
+  def render_responce
+    respond_to do |format|
+      format.html { render :index }
+      format.json do
+        txt = Russian.p(collection.count, 'товар', 'товара', 'товаров')
+        render json: { text: "Найдено #{collection.count} #{txt}" }
+      end
+    end
+  end
 
   def resource
     @product ||= Product.friendly.find(params[:id])
@@ -44,8 +57,9 @@ private
 
   def collection
     params[:brand_ids] ||= Brand.pluck(:id)
-    @products ||= Product.where(brand_id: params[:brand_ids])
-    @products.page(params[:page]).per(20)
+    @products ||= filter.result(distinct: true)
+                        .where(brand_id: params[:brand_ids])
+                        .page(params[:page]).per(20)
   end
 
   def selected_taxon
