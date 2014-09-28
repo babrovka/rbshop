eacc7572e6df4fc1e273bc1f49a8ba56d2c9516f
justvitalius <justvitalius@gmail.com>
2014-09-28 23:29:56 +0400

    Add gem for static pages.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,21 @@
+#
+# Данный контроллер необходим только для отрисовки главной страницы
+#
+# Если нужно создать еще пару статических страниц
+# то нужно воспользоваться гемом high_voltage
+# и просто поместить файл html.slim в папку pages
+# и в роутингах прописать адрес
+# никаких методов в контроллерах делать не нужно
+#
 class PagesController < ApplicationController
 
+  # главная страница
   def index
+    # для активации инверсной шапки на странице
     @extra_header = true
     @extra_products = Product.limit 10
     @slides = Slide.for_allowed_products
   end
 
-  def menu
-
-  end
 
 end
