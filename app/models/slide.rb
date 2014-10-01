4422e51b438a15ff3de85739e0897002c18f0f38
babrovka <babrovka@gmail.com>
2014-10-01 19:59:01 +0400

    slide scope

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,5 +1,6 @@
 class Slide < ActiveRecord::Base
   belongs_to :product
+  scope :for_allowed_products, -> { includes(:product).where(:products => {:show_in_slider => true}) }
   
   has_attached_file :image, styles: {slide: {geometry: "1200х700#"},
                                      slide_preview: {geometry: "100х100#"}
