cff13bd7fd891ddaeb91a1d7a7cbfa5b45b96d3e
justvitalius <justvitalius@gmail.com>
2014-10-05 00:28:25 +0400

    Render croped images in slides.

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -2,10 +2,13 @@ class Slide < ActiveRecord::Base
   belongs_to :product
   scope :for_allowed_products, -> { includes(:product).where(:products => {:show_in_slider => true}) }
   
-  has_attached_file :image, styles: {slide: {geometry: "1200х700#"},
-                                     slide_preview: {geometry: "100х100#"}
-                                     }
+  # has_attached_file :image, styles: {slide: {geometry: "1200х700#"},
+  #                                    slide_preview: {geometry: "100х100#"}
+  #                                    }
+  has_attached_file :image, styles: {slide: {geometry: "1200x700#"},
+                                     preview: {geometry: "100x100#"}
+  }
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
-  # crop_attached_file :image
+  crop_attached_file :image
 end
