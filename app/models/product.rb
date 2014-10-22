74974c1acbdc1ddcfd9ee101bc649113178b07f7
babrovka <babrovka@gmail.com>
2014-10-22 13:29:53 +0400

    filtered_by_taxons method

--- app/models/product.rb
+++ app/models/product.rb
@@ -72,6 +72,21 @@ class Product < ActiveRecord::Base
   accepts_nested_attributes_for :slide, allow_destroy: true
 
   after_update :reprocess_images
+  
+  def self.filtered_by_taxons(taxons)
+    joins = []
+    taxons.each_with_index.map do |taxon_id, i|
+      joins << "INNER JOIN shop_product_taxons up#{i} ON products.id = up#{i}.product_id"
+      joins << "INNER JOIN shop_taxons p#{i} ON p#{i}.id = up#{i}.taxon_id"
+    end.flatten!
+    
+    wheres = []
+    taxons.each_with_index.map do |taxon_id, i|
+      wheres << "p#{i}.id=#{taxon_id}"
+      wheres << "AND" unless i == taxons.count-1
+    end
+    joins(joins.join(' ')).where(wheres.join(' '))
+  end
 
   def reprocess_images
     self.product_images.each do |image|
